1c469c138dd199671480e491feae0dc0ae98afe2
Nick Cannariato <434063+nickcannariato@users.noreply.github.com>
2019-04-14 18:29:57 -0500

    :construction: Configure basic development server

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const morgan = require("morgan");
 
+// App definition
 const app = express();
 
 // Middleware:
-app.use(cors())
-app.use(express.json());
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(morgan("dev"));
 
 // Routes:
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Server is up" });
+});
 
-switch (process.env.NODE_ENV) {
-  case 'production':
-    app.listen(process.env.PORT)
-  case 'development':
-    app.listen(process.env.PORT, () => {
-      console.log(`Server is up @ http://localhost:${process.env.PORT}/`)
-    })
-}
+const server = app.listen(process.env.PORT, () => {
+  const host = server.address().address;
+  const port = server.address().port;
+
+  console.log(`API is up @ http://${host}:${port}`);
+});
