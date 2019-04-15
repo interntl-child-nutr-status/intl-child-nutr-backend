f23ffcd1744ed492181a53c55f6229b5b55c7b55
Nick Cannariato <434063+nickcannariato@users.noreply.github.com>
2019-04-15 12:24:51 -0500

    :construction: Update the listen method to reveal actual port in use

--- index.js
+++ index.js
@@ -4,14 +4,12 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const morgan = require("morgan");
 
-// App definition
 const app = express();
 
 // Middleware:
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
 app.use(morgan("dev"));
 
 // Routes:
@@ -19,9 +17,6 @@ app.get("/", (req, res) => {
   res.status(200).json({ message: "Server is up" });
 });
 
-const server = app.listen(process.env.PORT, () => {
-  const host = server.address().address;
-  const port = server.address().port;
-
-  console.log(`API is up @ http://${host}:${port}`);
+app.listen(process.env.PORT, () => {
+  console.log(`Dev server is up @ http://localhost:${process.env.PORT}/`);
 });
