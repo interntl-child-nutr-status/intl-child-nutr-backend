e2f9b237a7b38ed97a306ced4e7e270fa87bed18
Nick Cannariato <434063+nickcannariato@users.noreply.github.com>
2019-04-15 16:31:49 -0500

    :white_check_mark: Add create() method tests

--- __tests__/models/users.spec.js
+++ __tests__/models/users.spec.js
@@ -7,6 +7,23 @@ describe("User DB Model", () => {
     await db.seed.run()
     done()
   });
+  describe("create()", () => {
+    it("Creates a new user and returns that user", async () => {
+      const newUser = { 
+        username: "jaycannariato", 
+        password: bcrypt.hashSync('password', 10),
+        role_id: 2,
+        country_id: 230
+      }
+      const ins = await User.create(newUser);
+      expect(ins).toBeTruthy();
+      expect(ins.username).toBe('jaycannariato');
+      expect(bcrypt.compareSync('password', ins.password)).toBeTruthy();
+      expect(ins.id).toEqual(expect.any(Number));
+      expect(ins.role === 'Admin' || ins.role === 'User').toBeTruthy();
+      expect(ins.country_code).toBe('US');
+    });
+  })
     const users = await User.get();
     expect(users).toEqual(expect.any(Array));
   })
