faece9ed4d65ba0e645085ac34f2fd82283a07ce
Nick Cannariato <434063+nickcannariato@users.noreply.github.com>
2019-04-15 12:27:48 -0500

    :recycle: Update the database export for readability

--- models/index.js
+++ models/index.js
@@ -1,7 +1,9 @@
-const knex = require('knex')
-const dbConfig = require('../knexfile');
+const knex = require("knex");
+const dbConfig = require("../knexfile");
 
 const environment = process.env.NODE_ENV || "development";
 const configuration = dbConfig[environment];
+const db = knex(configuration)
 
-module.exports = knex(configuration);
+module.exports = db
+
