e3bfd7553dacd158c61469217abb8595b4bb5b42
Nick Cannariato <434063+nickcannariato@users.noreply.github.com>
2019-04-16 13:09:23 -0500

    :bug: Return country ID as part of the response

--- models/db/countries.js
+++ models/db/countries.js
@@ -1,13 +1,17 @@
 const db = require("../index");
 
 const get = () => {
-  return db("countries AS cn").select("cn.name AS Country", "cn.code AS Code");
+  return db("countries AS cn").select(
+    "cn.id AS id",
+    "cn.name AS Country",
+    "cn.code AS Code"
+  );
 };
 
 const getActive = countryCode => {
   if (countryCode) {
     return db("countries AS cn")
-      .select("cn.name AS Country")
+      .select("cn.id as id", "cn.name AS Country")
       .count("cm.id AS Communities")
       .join("communities AS cm", { "cm.country_id": "cn.id" })
       .groupBy("cn.name")
@@ -15,7 +19,7 @@ const getActive = countryCode => {
       .where({ "cn.code": countryCode });
   }
   return db("countries AS cn")
-    .select("cn.name AS Country")
+    .select("cn.id as id", "cn.name AS Country")
     .count("cm.id AS Communities")
     .join("communities AS cm", { "cm.country_id": "cn.id" })
     .groupBy("cn.name")
