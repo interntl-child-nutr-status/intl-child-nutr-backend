d88676854ab1e46b5789bc99d5148279fb332e27
Nick Cannariato <434063+nickcannariato@users.noreply.github.com>
2019-04-16 18:35:04 -0500

    :sparkles: Enable the /:country_id/:community_id endpoint

--- controllers/community.js
+++ controllers/community.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
 const Community = require("../models/db/communities");
+const Children = require('../models/db/children');
 
-router.route("/:country_id")
+router
+  .route("/:country_id")
   .get(async (req, res) => {
     const { country_id } = req.params;
     try {
@@ -27,23 +29,91 @@ router.route("/:country_id")
     if (!name || !city) {
       return res.status(400).json({
         message: "Name and City properties are required"
-      })
+      });
     }
     try {
       const newCommunity = await Community.create({
         name,
         city,
         country_id
-      })
+      });
 
       res.status(201).json(newCommunity);
-    }
-    catch (e) {
+    } catch (e) {
       console.log(e);
       return res.status(500).json({
         message: "Sorry! We ran into a problem creating that community"
       });
     }
   });
+
+router
+  .route("/:country_id/:id")
+  .get(async (req, res) => {
+    const { country_id, id } = req.params;
+    try {
+      const community = await Community.get(country_id, id);
+      const children = await Children.get(id, null)
+
+      if (!community) {
+        return res.status(404).json({
+          message: "There doesn't appear to be a community at that ID"
+        });
+      }
+
+      return res.status(200).json({
+        ...community,
+        children
+      });
+    } catch (e) {
+      console.error(e);
+      res.status(500).json({
+        message:
+          "Sorry! We encoutered an error fetching that community from the database."
+      });
+    }
+  })
+  .put(async (req, res) => {
+    const { country_id, id } = req.params;
+    const changes = req.body;
+    if (!changes.name.length || !changes.city.length) {
+      return res.status(400).json({
+        message: "You must include a change to either the name or a city"
+      })
+    }
+    try {
+      const update = await Community.update(country_id, id, changes)
+
+      return res.status(200).json(update)
+    }
+    catch (e) {
+      console.log(e);
+      return res.status(500).json({
+        message: "Sorry! We encountered an error updating that Community"
+      })
+    }
+  })
+  .delete(async (req, res) => {
+    const { country_id, id } = req.params;
+    try {
+      const community = await Community.get(country_id, id);
+
+      if (!community) {
+        return res.status(404).json({
+          message: "We can't seem to find a community at that ID"
+        })
+      }
+
+      await Community.remove(id);
+
+      return res.status(200).json(community);
+    }
+    catch (e) {
+      console.error(e);
+      return res.status(500).json({
+        message: "Sorry! We encountered an error while deleting that Community"
+      })
+    }
+  })
 
 module.exports = router;
