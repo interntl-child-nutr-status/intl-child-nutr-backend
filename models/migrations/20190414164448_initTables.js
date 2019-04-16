5628d29dc5d9a5e28c0a648d6bc10cd58b61d639
Nick Cannariato <434063+nickcannariato@users.noreply.github.com>
2019-04-16 11:22:53 -0500

    :card_file_box: Simplify child/parent relationship for now

--- models/migrations/20190414164448_initTables.js
+++ models/migrations/20190414164448_initTables.js
@@ -5,7 +5,7 @@ exports.up = function(knex) {
       tbl.string("name", 255)
          .unique()
          .notNullable();
-      tbl.string('code', 2)
+      tbl.string("code", 2)
          .unique()
          .notNullable();
     })
@@ -21,38 +21,19 @@ exports.up = function(knex) {
     })
     .createTable("children", tbl => {
       tbl.increments();
-      tbl.string("name", 255)
-         .notNullable();
-      tbl.date("dob")
-         .notNullable();
-      tbl.integer("age")
-         .notNullable();
+      tbl.string("name", 255).notNullable();
+      tbl.date("dob").notNullable();
+      tbl.integer("age").notNullable();
       tbl.integer("height");
       tbl.integer("weight");
-      tbl.enu("sex", ["M", "F"], {
-        useNative: true,
-        enumName: "child_sex"
-      });
-    })
-    .createTable('parents', tbl => {
-      tbl.increments();
-      tbl.string('name')
-         .unique()
-         .notNullable()
-      tbl.string('address')
-    })
-    .createTable('parents_children', tbl => {
-      tbl.increments();
-      tbl.integer('parent_id')
-         .references('id')
-         .inTable('parents')
-         .onDelete('CASCADE')
-         .onUpdate('CASCADE')
-      tbl.integer('child_id')
-         .references('id')
-         .inTable('children')
-         .onDelete('CASCADE')
-         .onUpdate('CASCADE')
+      tbl.string("sex", 1);
+      tbl.string("primary_parent", 255)
+      tbl.string("address", 255)
+      tbl.integer("community_id")
+         .references("id")
+         .inTable("communities")
+         .onDelete("CASCADE")
+         .onUpdate("CASCADE")
     })
     .createTable("screenings", tbl => {
       tbl.increments();
@@ -62,47 +43,39 @@ exports.up = function(knex) {
          .inTable("children")
          .onDelete("CASCADE")
          .onUpdate("CASCADE");
-      tbl.integer("community_id")
-         .references('id')
-         .inTable('communities')
-         .onDelete('CASCADE')
-         .onUpdate('CASCADE')
     })
-    .createTable('roles', tbl => {
-      tbl.increments()
-      tbl.string('title', 255)
+    .createTable("roles", tbl => {
+      tbl.increments();
+      tbl.string("title", 255)
          .unique()
-         .notNullable()
-      tbl.string('description', 255)
+         .notNullable();
+      tbl.string("description", 255);
     })
-    .createTable('users', tbl => {
-      tbl.increments()
-      tbl.string('username', 255)
+    .createTable("users", tbl => {
+      tbl.increments();
+      tbl.string("username", 255)
          .unique()
-         .notNullable()
-      tbl.string('password', 255)
-         .notNullable()
-      tbl.integer('role_id')
-         .references('id')
-         .inTable('roles')
-         .onDelete('CASCADE')
-         .onUpdate('CASCADE')
-      tbl.integer('country_id')
-         .references('id')
-         .inTable('countries')
-         .onDelete('CASCADE')
-         .onUpdate('CASCADE')
-    })
+         .notNullable();
+      tbl.string("password", 255).notNullable();
+      tbl.integer("role_id")
+         .references("id")
+         .inTable("roles")
+         .onDelete("CASCADE")
+         .onUpdate("CASCADE");
+      tbl.integer("country_id")
+         .references("id")
+         .inTable("countries")
+         .onDelete("CASCADE")
+         .onUpdate("CASCADE");
+    });
 };
 
 exports.down = function(knex) {
   return knex.schema
-    .dropTableIfExists('users')
-    .dropTableIfExists('roles')
-    .dropTableIfExists('screenings')
-    .dropTableIfExists('parents_children')
-    .dropTableIfExists('parents')
-    .dropTableIfExists('children')
-    .dropTableIfExists('communities')
-    .dropTableIfExists('countries')
+   .dropTableIfExists("users")
+   .dropTableIfExists("roles")
+   .dropTableIfExists("screenings")
+   .dropTableIfExists("children")
+   .dropTableIfExists("communities")
+   .dropTableIfExists("countries")
 };
--- models/seeds/000-cleanup.js
+++ models/seeds/000-cleanup.js
@@ -1,5 +1,5 @@
 exports.seed = function(knex) {
   return knex.schema.raw(
-    "TRUNCATE countries, communities, children, parents, parents_children, screenings, roles, users RESTART IDENTITY;"
+    "TRUNCATE countries, communities, children, screenings, roles, users RESTART IDENTITY;"
   );
 };
