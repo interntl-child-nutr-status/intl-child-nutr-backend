f5f1adfa4a69edec8b0bf2c90f0a7d613d01fd73
Nick Cannariato <434063+nickcannariato@users.noreply.github.com>
2019-04-17 00:36:27 -0500

    :card_file_box: Add get() method to Child model

--- models/db/children.js
+++ models/db/children.js
@@ -1,12 +1,14 @@
-const db = require('../index');
+const db = require("../index");
+const moment = require("moment");
 
 const get = (community_id, id) => {
   if (id) {
-    return db('children').where({ id }).first();
+    return db("children")
+      .where({ id })
+      .first();
   }
-  return db('children')
-    .where({ community_id });
-}
+  return db("children").where({ community_id });
+};
 
 module.exports = {
   get,
