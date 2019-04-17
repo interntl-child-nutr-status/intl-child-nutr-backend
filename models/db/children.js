264f627873d4895f8a3daa218ff36744d9b214e8
Nick Cannariato <434063+nickcannariato@users.noreply.github.com>
2019-04-17 00:36:56 -0500

    :card_file_box: Add create() method to Child model

--- models/db/children.js
+++ models/db/children.js
@@ -10,6 +10,23 @@ const get = (community_id, id) => {
   return db("children").where({ community_id });
 };
 
+const create = child => {
+  const newChild = {
+    name: child.name,
+    contact: child.contact,
+    guardian: child.guardian,
+    community_id: parseInt(child.community),
+    country_id: parseInt(child.country),
+    age: moment(Date.now()).diff(
+      moment(child.dateOfBirth, "MMDDYYYY"),
+      "months"
+    ),
+    dob: moment(child.dateOfBirth, "MMDDYYYY")
+  };
+  return db("children")
+    .insert(newChild, ["id"])
+    .then(c => get(c.community_id, c[0].id));
+};
 module.exports = {
   get,
 }
